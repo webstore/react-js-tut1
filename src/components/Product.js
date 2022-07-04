d7b9f51de8e3d91790edbfd1c719df0deb204a66
webstore <somak.kanjilal@gmail.com>
2022-07-04 21:19:38 +0530

    1 file modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,17 @@
-const Product = () => {};
+const Product = () => {
+  return;
+  <article className="m-5 p-2">
+    <div className="row">
+      <div class="col-lg-2 col-md-2 col-sm-2">
+
+      </div>
+      <div class="col-lg-5 col-md-5 col-sm-5">
+        
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        
+      </div>
+
+    </div>
+  </article>;
+};
