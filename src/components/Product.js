a28f7fe19c05109243360bf9f8d7b3ea37934a96
webstore <somak.kanjilal@gmail.com>
2022-07-04 21:27:45 +0530

    1 file modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,23 @@
 const Product = () => {
-  return;
-  <article className="m-5 p-2">
-    <div className="row">
-      <div className="col-lg-2 col-md-2 col-sm-2">
-        <img src="" width="250" />
-      </div>
-      <div className="col-lg-5 col-md-5 col-sm-5">
-        <div className="text-left">Entertainment equipment</div>
-      </div>
-      <div className="col-lg-3 col-md-3 col-sm-3">
-        <div className="text-right">
-          In marketing, a product is an object, or system, or service made
-          available for consumer use as of the consumer demand
+  return (
+    <article className="m-5 p-2">
+      <div className="row">
+        <div className="col-lg-2 col-md-2 col-sm-2">
+          <img src="" width="250" />
+        </div>
+        <div className="col-lg-5 col-md-5 col-sm-5">
+          <div className="text-left">Entertainment equipment</div>
+        </div>
+        <div className="col-lg-3 col-md-3 col-sm-3">
+          <div className="text-right">
+            In marketing, a product is an object, or system, or service made
+            available for consumer use as of the consumer demand
+          </div>
+        </div>
+        <div className="col-lg-1 col-md-1 col-sm-1">
+          <span>$ 250</span>
         </div>
       </div>
-      <div className="col-lg-1 col-md-1 col-sm-1">
-        <span>$ 250</span>
-      </div>
-    </div>
-  </article>;
+    </article>
+  );
 };
