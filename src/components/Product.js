311aeb0eaabcbe38c21a710b64e4af7122a353c8
webstore <somak.kanjilal@gmail.com>
2022-07-04 22:20:09 +0530

    2 files modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,21 @@
 import React from 'react';
-const Product = () => {
+
+const Product = ({ id, name, description, price, img }) => {
   return (
-    <div className="col-lg-3 col-md-12 col-sm-12 m-2 border">
+    <div className="col-lg-3 col-md-12 col-sm-12 m-2 border" id={id}>
       <div className="row">
         <div className="col-lg-3 col-md-3 col-sm-12">
           <div className="text-left mb-1">
-            <img
-              className="border border-primary m-1"
-              src="https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg"
-              width="100"
-            />
+            <img className="border border-primary m-1" src={img} width="100" />
           </div>
         </div>
         <div className="col-lg-8 col-md-5 col-sm-6">
-          <h6 className="text-left">Entertainment equipment</h6>
+          <h6 className="text-left">{name}</h6>
         </div>
         <div className="col-lg-12 col-md-5 col-sm-6">
-          <div className="text-left text-secondary">
-            In marketing, a product is an object, or system, or service made
-            available for consumer use as of the consumer demand
-          </div>
+          <div className="text-left text-secondary">{description}</div>
           <div className=" h6 text-primary">
-            <span>$ 550</span>
+            <span>{price}</span>
             <button className="ml-4 btn btn-primary btn-xs">Add to Cart</button>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -7,14 +7,59 @@ import Product from './components/Product';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+const products = [
+  {
+    id: 1,
+    name: 'Entertainment equipment',
+    description:
+      'In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand',
+    price: '$100',
+    img: 'https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg',
+  },
+
+  {
+    id: 2,
+    name: 'Second equipment',
+    description:
+      'In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand',
+    price: '$200',
+    img: 'https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg',
+  },
+
+  {
+    id: 3,
+    name: 'Second equipment',
+    description:
+      'In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand',
+    price: '$200',
+    img: 'https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg',
+  },
+
+  {
+    id: 4,
+    name: 'Second equipment',
+    description:
+      'In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand',
+    price: '$200',
+    img: 'https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg',
+  },
+
+  {
+    id: 5,
+    name: 'Second equipment',
+    description:
+      'In marketing, a product is an object, or system, or service made available for consumer use as of the consumer demand',
+    price: '$200',
+    img: 'https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg',
+  },
+];
 
 root.render(
   <StrictMode>
     <div className="row">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {products.map((prdItem) => (
+        <Product key={prdItem.id} {...prdItem} />
+      ))}
     </div>
   </StrictMode>
 );
