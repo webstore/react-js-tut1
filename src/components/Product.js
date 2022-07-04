baa1400949bacdd5b7f522eab04014c1ca2ba546
webstore <somak.kanjilal@gmail.com>
2022-07-04 21:42:11 +0530

    1 file modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,20 +3,25 @@ const Product = () => {
   return (
     <article className="m-5 p-2">
       <div className="row">
-        <div className="col-lg-1 col-md-1 col-sm-1">
-          <img src="" width="250" />
+        <div className="col-lg-1 col-md-1 col-sm-2">
+          <div className="text-left mb-1">
+            <img
+              className="border border-primary"
+              src="https://raw.githubusercontent.com/webstore/react-js-tut1/main/src/assets/images/download.jpg"
+              width="150"
+            />
+          </div>
         </div>
         <div className="col-lg-3 col-md-3 col-sm-5">
-          <h3 className="text-left">Entertainment equipment</h3>
-        </div>
-        <div className="col-lg-3 col-md-3 col-sm-3">
-          <div className="text-left">
+          <h6 className="text-left ml-1">Entertainment equipment</h6>
+          <div className="text-left ml-1 text-secondary">
             In marketing, a product is an object, or system, or service made
             available for consumer use as of the consumer demand
           </div>
         </div>
+
         <div className="col-lg-1 col-md-1 col-sm-1">
-          <span>$ 250</span>
+          <div className=" h6 text-primary  ml-1">$ 250</div>
         </div>
       </div>
     </article>
